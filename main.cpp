905aef750880faa8fb37602821041efebf6c5b01
BuXiu790 <buxiu790@gmail.com>
2026-04-20 13:53:15 +0800

    feat: init Linux C++ workspace

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int target = 1024;
+    std::cout << "Hello, Linux System!" << std::endl;
+    std::cout << "Variable 'target' is stored at memory address: " << &target << std::endl;
+    return 0;
+}
